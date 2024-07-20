9d1ddb37a037c92040d42e2cd7435953d788d5da
Orion Serup <orion@crablabs.io>
2024-07-19 18:45:08 -0700

    added analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercelStatic from "@astrojs/vercel/static";
+import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://crablabs.io",
+  integrations: [
+    tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    } ),
+    sitemap(), 
+    mdx()
+  ],
   output: "static",
   adapter: vercelStatic()
 });
